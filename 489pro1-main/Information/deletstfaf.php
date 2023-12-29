64609e0bba1f519e4ea3d6522f1cc694c62786fe
Fadelm300 <148485362+Fadem300@users.noreply.github.com>
2023-12-29 15:25:43 +0300

    Update deletstfaf.php

--- 489pro1-main/Information/deletstfaf.php
+++ 489pro1-main/Information/deletstfaf.php
@@ -3,7 +3,7 @@ include "connection.php";
 
 
 //delet  pharmacist
-$Employee_ID= $_GET['DEEL'];
+$Employee_ID= $_GET['SDEEL'];
 $query = "DELETE FROM pharmacist WHERE Employee_ID='".$Employee_ID."'";
 $result=mysqli_query($con,$query);
 if($result){
