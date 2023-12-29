0175102ad74ed23ded8249422ea1ef2509c16a91
Fadelm300 <148485362+Fadem300@users.noreply.github.com>
2023-12-29 15:23:24 +0300

    Update viewAdmin.php

--- 489pro1-main/Information/viewAdmin.php
+++ 489pro1-main/Information/viewAdmin.php
@@ -279,7 +279,7 @@ while ($row = $r->fetch()) {
                         <td><a href="#">Edit</a></td>
 
                         <td>
-              <a href="delete3.php?DEEL=<?php echo $row['Employee_ID']?>" class="link-dark"><i class="fa-solid fa-trash
+              <a href="delete3.php?ADEEL=<?php echo $row['Employee_ID']?>" class="link-dark"><i class="fa-solid fa-trash
               fs-5 me-3"></i></a>
                         </td>
                      
