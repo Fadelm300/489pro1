d89bf32223dc94937bbec88d923607af41f5cb26
Fadelm300 <148485362+Fadem300@users.noreply.github.com>
2023-12-30 01:10:03 +0300

    Update homeforadmen.php
    search in home admin page

--- 489pro1-main/Information/homeforadmen.php
+++ 489pro1-main/Information/homeforadmen.php
@@ -8,6 +8,37 @@
 
     <script src="https://kit.fontawesome.com/ba22c05506.js" crossorigin="anonymous"></script>
     <title>HOME page</title>
+    <style>
+        .containerhome{
+    background: #c2c8ec;
+    border-radius: 30px;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.35);
+    overflow: hidden;
+    width: 1000px;
+    max-width: 100%;
+    min-height: 480px;
+    margin-top: 10px;
+}
+.conteneras1 {
+   width: 100%;
+   display: flex;
+   flex-direction: row;
+   flex-wrap: wrap;
+   justify-content: center;
+   align-items: center;
+}
+
+.centered-div {
+   width: 40%;
+   height: 100px;
+   margin: 10px;
+   padding: 5px;
+   color: white;
+}
+h4{
+    align-self: center;
+}
+    </style>
 </head>
 <body>
 <div class="container">
@@ -167,10 +198,99 @@
             <form enctype="multipart/form-data" method="post">
      
             
-                       <div class="containerInsid">
+                       <div class="containerhome">
+<div class="conteneras1">
+    <div class="centered-div">
+    
+                                    <div class="h4" ><h4>   HOME page</h4></div>
 
-                                    <div class="h4"><h4>   HOME page</h4></div>
+                                    <form action="SearchMed.php" method="POST">
+        <h1>Search for a Medicine:</h1>
+        <input type="text" name="search">
+        <input type="submit"  name="btn" style="color: black" >
+    </form>
+</div></div>
+    <?php 
+if(isset($_POST['btn'])){
+?>
 
+
+
+<table border=1>
+    <tr >
+        <th>Medicine ID</th>
+        <th>Medicine Name</th>
+        <th>Medicine Price</th>
+        <th>Medicine Quantity</th>
+        <th>Medicine dateOfCreate and end</th>
+        <th>Medicine theProducer</th>
+        <th>Medicine Supplier phone number</th>
+        <th>description </th>
+
+       
+
+
+    </tr>
+                        <tr >
+                        <?php
+                        $search=$_POST['search'];
+                        include"connection.php";
+                        $query= "SELECT * FROM medicine  WHERE  medicine_name         LIKE '%$search%' 
+                                                                OR medicine_ID LIKE '%$search%'  
+                                                                OR price  LIKE '%$search%' 
+                                                                OR quantity  LIKE '%$search%' 
+                                                                OR   dateOfCreate LIKE '%$search%'       
+                                                                OR dateOfEnd  LIKE '%$search%'
+                                                                OR theProducer  LIKE '%$search%'
+                                                                OR  Supplierphonenumber LIKE '%$search%'
+                                                                           "; 
+                        $result=mysqli_query($con,$query);
+            
+
+                            while($row = mysqli_fetch_assoc($result))
+                        {?>
+                        <td> <h6><?php echo $row['medicine_ID'] ?></h6></td>
+
+                        <td><h6><?php echo $row['medicine_name'] ?></h6></td>
+
+                        <td><h6><?php echo $row['price'] ?></h6></td>
+
+                        <td><h6><?php echo $row['quantity'] ?></h6></td>
+
+                        <td><h6><?php echo $row['dateOfCreate']?>//<?php echo $row['dateOfEnd']  ?></h6></td>
+
+                        <td><h6><?php echo $row['theProducer'] ?></h6></td>
+
+                        <td><h6>(<?php echo $row['key']?> )<?php echo $row['Supplierphonenumber']  ?></h6></td>
+                        
+
+                        <td><h6><?php echo $row['description'] ?></h6></td>
+
+
+                        <td><a href="#">Edit</a></td>
+
+                        <td>
+              <a href="delete3.php?DEEL=<?php echo $row['medicine_ID']?>" class="link-dark"><i class="fa-solid fa-trash
+              fs-5 me-3"></i></a>
+                        </td>
+
+
+
+
+                                </tr>
+                        <?php
+                            }
+                            ?>
+
+                       
+
+
+</table>
+
+
+<?php 
+/*if btn */
+}?>
 
 
 </div>
